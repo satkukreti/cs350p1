15a58e883b5e8ba130864e62084a03519c5a9b40
Satvik Kukreti <skukret1@cs.binghamton.edu>
2024-02-25 01:45:01 -0500

    extra credit done

---
+++ base/sysproc.c
@@ -0,0 +1,113 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int sys_iqtest(void){
+  char* msg;
+  argstr(0, &msg);
+  int sum = 0;
+  int counter = 0;
+  while(*msg != '\0'){
+    sum += (*msg % 21);
+    msg++;
+    counter++;
+  }
+  sum /= counter;
+  cprintf("Your IQ is %d\n", sum);
+  if(sum < 11){
+    cprintf("Unfortunately, this means you are not smart :(\n");
+    cprintf("Don't worry though, try to find the pattern and try again\n");
+  } else {
+    cprintf("Congradulations, you are big brained! :)\n");
+  }
+  return 0;
+  
+}
